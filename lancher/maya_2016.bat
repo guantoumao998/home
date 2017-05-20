cee6423c767ea8a1dfa79985ae5b828abbc5f95c
wuxiaomeng <alucard_998@hotmail.com>
2017-05-21 04:16:02 +0800

    third

--- lancher/maya_2016.bat
+++ lancher/maya_2016.bat
@@ -2,9 +2,9 @@
 
 @echo 载入环境变量...
 SET MAYA_UI_LANGUAGE=en_US
-SET MAYA_SCRIPT_PATH = %MAYA_SCRIPT_PATH%C:\Users\www998www\Documents\maya\python\mel_script;
-SET PYTHONPATH=%PYTHONPATH%C:\PyMaya\Lib;C:\Users\www998www\Documents\maya\python;
-SET PYTHONPATH=%PYTHONPATH%C:\PyMaya\Lib\win32;C:\PyMaya\Lib\win32\lib;C:\PyMaya\Lib\pythonwin;
+SET MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;C:\Users\www998www\Documents\maya\python\mel_script
+SET PYTHONPATH=%PYTHONPATH%;C:\PyMaya\Lib;C:\Users\www998www\Documents\maya\python
+SET PYTHONPATH=%PYTHONPATH%;C:\Users\www998www\Documents\maya\python\third_module
 
 @echo 启动Maya主程序...
 maya.exe *
